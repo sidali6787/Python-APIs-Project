33d028b65c6c164fc10090b12ff5e1bbfef88cf8
Asif Ali <asifali@asifs-mbp.myfiosgateway.com>
2021-01-18 17:09:47 -0500

    2nd push

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "37cf4f0c4a2f8de6306a74142fec866e"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCaoMmbBFemWfeNZ7eaC9Lf9CrLeA82eyo"
